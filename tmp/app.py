2ef81907ab7e3ce55be4aebabe84903e08a884aa
RchLi <lyric0mel@gmail.com>
2022-12-05 18:59:07 -0500

    modity @cache

--- tmp/app.py
+++ tmp/app.py
@@ -5,19 +5,19 @@ from metaflow import get_metadata, metadata
 import pandas as pd
 import numpy as np
 
-@st.cache
-def get_latest_successful_run(flow_name: str):
-    "Gets the latest successfull run."
-    for r in Flow(flow_name).runs():
-        if r.successful: 
-            return r
+# @st.cache
+# def get_latest_successful_run(flow_name: str):
+#     "Gets the latest successfull run."
+#     for r in Flow(flow_name).runs():
+#         if r.successful: 
+#             return r
 
 if __name__ == '__main__':
     FLOW_NAME = 'MyClassificationFlow'
     namespace(None)
     metadata('./tmp')
     print(get_metadata())
-    latest_run = get_latest_successful_run(FLOW_NAME)
+    latest_run = Flow(FLOW_NAME).latest_successful_run
     model = latest_run.data.svm_Jap
     vectorizer = latest_run.data.vectorizer_Jap
     positive_words = latest_run.data.positive_words
