e1e9181e57037dc308f9641166864e1a4928e4b1
RchLi <lyric0mel@gmail.com>
2022-12-07 16:45:20 -0500

    App, add restaurant distribution

--- tmp/app.py
+++ tmp/app.py
@@ -1,35 +1,43 @@
+from math import dist
 import streamlit as st
 # from metaflow import Flow, namespace
 # from metaflow import get_metadata, metadata
 # import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 import numpy as np
 import os
 import pickle
-# @st.cache
-# def get_latest_successful_run(flow_name: str):
-#     for r in Flow(flow_name).runs():
-#         #if r.successful: 
-#         return r
+import matplotlib.pyplot as plt
+
+def plot_distribution(data):
+    fig, ax = plt.subplots(figsize=(11, 7))
+    sns.barplot(y=data.index, x= data.values, palette= sns.color_palette("RdBu_r", len(data)),ax=ax)
+    ax.set_ylabel('Category', fontsize=14)
+    ax.set_xlabel('Count of reviews', fontsize=14)
+    ax.set_title('Count of Reviews by Cuisine Type', fontsize=15)
+    for i,v in enumerate(data):
+        ax.text(v, i+0.15, str(v),fontweight='bold', fontsize=14)
+    ax.tick_params(labelsize=14)
+
+    return fig
 
 if __name__ == '__main__':
-    # FLOW_NAME = 'MyClassificationFlow'
-    # namespace(None)
-    # metadata('./tmp')
-    # get_metadata()
-    # latest_run = Flow(FLOW_NAME).latest_successful_run
-    # model = latest_run.data.svm_Jap
-    # vectorizer = latest_run.data.vectorizer_Jap
-    # positive_words = latest_run.data.positive_words
-    # negative_words = latest_run.data.negative_words
-
-    # Directly load from pickle
+    # information
+    st.markdown("# Restaurant Information")
+    distribution = pd.read_csv('./data/distribution_by_category.csv', index_col=[0])
+    category = distribution.index
+    x = st.selectbox('Choose the type of your restaurant', category)
+    fig = plot_distribution(distribution.loc[x].sort_values(ascending=False))
+    st.pyplot(fig)
+    
+    # model prediction
+    st.markdown("# Review Sentiment Analysis")
+    # load model with pickle
     with open('./models/svm.pkl', 'rb') as f:
         model = pickle.load(f)
     with open('./models/vec.pkl', 'rb') as f:
         vectorizer = pickle.load(f)
-
-    st.markdown("# Review Sentiment Analysis")
     review = st.text_input('Your Review', '')
     if review == '':
         label = ''
