b0d7f7385dc76a542fda6178c02be9bbe03f3f5c
RchLi <lyric0mel@gmail.com>
2022-12-07 21:38:19 -0500

    App, add wordcloud

--- pages/1_Restaurant_Information.py
+++ pages/1_Restaurant_Information.py
@@ -5,13 +5,13 @@ import numpy as np
 import os
 import pickle
 import matplotlib.pyplot as plt
-
+from wordcloud import WordCloud
 def state_distribution(data):
     data = data.sort_values(ascending=False)
     fig, ax = plt.subplots(figsize=(11, 7))
     sns.barplot(y=data.index, x= data.values, palette= sns.color_palette("RdBu_r", len(data)),ax=ax)
-    ax.set_ylabel('Category', fontsize=14)
-    ax.set_xlabel('Number of Restaurants', fontsize=14)
+    ax.set_ylabel('State', fontsize=14)
+    ax.set_xlabel('Number', fontsize=14)
     ax.set_title('Number of Restaurants by State', fontsize=15)
     for i,v in enumerate(data):
         ax.text(v, i+0.15, str(v),fontweight='bold', fontsize=14)
@@ -35,18 +35,40 @@ def city_distribution(data, k=15):
     
     return fig
 
+def word_img(words):
+    text = ' '.join(words)
+    wordcloud = WordCloud(collocations = False, background_color = 'white').generate(text)
+    fig, ax = plt.subplots(figsize=(11, 7))
+    ax.imshow(wordcloud)
+    plt.axis("off")
+    #plt.tight_layout(pad = 0)
+    return fig
+
 if __name__ == '__main__':
-    st.set_page_config(
-        page_title="Restaurant Information",
-    )
+    st.set_page_config(page_title="Restaurant Information",layout='wide')
+
     st.sidebar.header("Restaurant Information")
-    st.markdown("# Restaurant Information")
+
+    col1, col2 = st.columns(2)
     state = pd.read_csv('./data/distribution_state.csv', index_col=[0])
     city = pd.read_csv('./data/distribution_city.csv', index_col=[0])
-    category = state.index
-    x = st.selectbox('Choose the type of your restaurant', category)
-    fig = state_distribution(state.loc[x])
-    st.pyplot(fig)
-    fig = city_distribution(city.loc[x])
-    st.pyplot(fig)
+    with open('./data/cusine_words','rb') as f:
+        cusine_words = pickle.load(f)
+
+    with col1:
+        st.markdown("# Restaurant Information")
+        category = state.index
+        x = st.selectbox('Choose the type of your restaurant', category)
+
+        st.markdown('## Important factors for %s restaurant' % (x))
+        fig = word_img(cusine_words[x])
+        st.pyplot(fig)
+
+    with col2:
+        st.markdown('## Distribution of %s restaurant' % (x))
+        fig = state_distribution(state.loc[x])
+        st.pyplot(fig)
+        fig = city_distribution(city.loc[x])
+        st.pyplot(fig)
+  
     
